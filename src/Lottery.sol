40e8214a3b0ba067d057982407b6b89f42935301
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-10-10 16:47:48 +0300

    Add Receive and Fallback functions

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -8,17 +8,28 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Lottery {
+    /**
+        Erros
+     */
+    error NotEnoughEthToEnterLottery();
+
     uint256 private immutable i_entrancePrice;
 
     constructor(uint256 _entrancePrice) {
         i_entrancePrice = _entrancePrice;
     }
 
+    receive() external payable {}
+
+    fallback() external payable {}
+
     function enterLottery() public payable {}
 
     function pickWinner() public {}
 
-    /** View / Pure functions */
+    /** 
+        View / Pure functions 
+    */
 
     function getEntrancePrice() external view returns (uint256) {
         return i_entrancePrice;
