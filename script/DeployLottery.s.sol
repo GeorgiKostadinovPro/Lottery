228a2d5ae26e6c6bcec1a4ca9a20f5842dabc950
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-10-10 16:28:49 +0300

    Create Base Functions Structure

---
+++ script/DeployLottery.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+
+contract DeployLottery is Script {
+    function run() public {}
+}
---
+++ src/Lottery.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+/**
+ * @title Lottery contract
+ * @author Georgi Kostadinov / 0xAlipede
+ * @notice A simple automated contract for Lottery
+ * @dev Implements Chainlink VRFv2.5
+ */
+contract Lottery {
+    uint256 private immutable i_entrancePrice;
+
+    constructor(uint256 _entrancePrice) {
+        i_entrancePrice = _entrancePrice;
+    }
+
+    function enterLottery() public payable {}
+
+    function pickWinner() public {}
+
+    /** View / Pure functions */
+
+    function getEntrancePrice() external view returns (uint256) {
+        return i_entrancePrice;
+    }
+}
