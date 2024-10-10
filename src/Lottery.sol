6f891b4d4289bfa3b285f8e766cf5073d1663499
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-10-10 22:15:07 +0300

    Reset Lottery State when winner is chosen

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -11,16 +11,16 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/V
  * @dev Implements Chainlink VRFv2.5
  */
 contract Lottery is VRFConsumerBaseV2Plus {
-    /**
-        Erros
-    */
     error Lottery__NotEnoughEthToEnterLottery();
     error Lottery__NotEnoughTimePassedToPickWinner();
     error Lottery__UnsuccessfulRewardTranfer();
+    error Lottery__ChoosingWinner();
 
-    /**
-        State Variables
-    */
+    enum LotteryState {
+        OPEN,
+        CHOOSING_WINNER
+    }
+
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
@@ -33,14 +33,11 @@ contract Lottery is VRFConsumerBaseV2Plus {
     uint256 private s_lastTimeWinnerPicked;
     address payable[] private s_participants;
 
-    /**
-        Events
-    */
-    event EnterLottery(address indexed participant);
+    LotteryState private s_lotteryState;
 
-    /**
-        Functions
-    */
+    event EnterLottery(address indexed participant);
+    event ChosenWinner(address indexed winner);
+
     constructor(
         uint256 _entrancePrice,
         uint256 _lotteryDuration,
@@ -56,6 +53,8 @@ contract Lottery is VRFConsumerBaseV2Plus {
         i_keyHash = _keyHash;
         i_subscriptionId = _subscriptionId;
         i_callbackGasLimit = _callbackGasLimit;
+
+        s_lotteryState = LotteryState.OPEN;
     }
 
     receive() external payable {}
@@ -67,14 +66,20 @@ contract Lottery is VRFConsumerBaseV2Plus {
             revert Lottery__NotEnoughEthToEnterLottery();
         }
 
+        if (s_lotteryState == LotteryState.CHOOSING_WINNER) {
+            revert Lottery__ChoosingWinner();
+        }
+
         s_participants.push(payable(msg.sender));
         emit EnterLottery(msg.sender);
     }
 
-    function pickWinner() external {
+    function chooseWinner() external {
         if ((block.timestamp - s_lastTimeWinnerPicked) < i_lotteryDuration) {
             revert Lottery__NotEnoughTimePassedToPickWinner();
         }
+
+        s_lotteryState = LotteryState.CHOOSING_WINNER;
 
         uint256 requestId = s_vrfCoordinator.requestRandomWords(
             VRFV2PlusClient.RandomWordsRequest({
@@ -97,16 +102,18 @@ contract Lottery is VRFConsumerBaseV2Plus {
     ) internal override {
         uint256 winnerIndex = randomWords[0] % s_participants.length;
         address payable winner = s_participants[winnerIndex];
-        (bool success, ) = winner.call{value: address(this).balance}("");
 
+        s_lotteryState = LotteryState.OPEN;
+        s_lastTimeWinnerPicked = block.timestamp;
+        s_participants = new address payable[](0);
+
+        (bool success, ) = winner.call{value: address(this).balance}("");
         if (!success) {
             revert Lottery__UnsuccessfulRewardTranfer();
         }
-    }
 
-    /** 
-        View / Pure functions 
-    */
+        emit ChosenWinner(winner);
+    }
 
     function getEntrancePrice() external view returns (uint256) {
         return i_entrancePrice;
