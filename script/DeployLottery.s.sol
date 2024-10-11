f92452ca6c45dd23876e8dc44fa968adde150061
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-10-12 00:43:10 +0300

    Create Subscription to Consumer programatically

--- script/DeployLottery.s.sol
+++ script/DeployLottery.s.sol
@@ -4,13 +4,18 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Lottery} from "../src/Lottery.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {CreateSubscription} from "./Interactions.s.sol";
 
 contract DeployLottery is Script {
     function deployLottery() public returns (Lottery, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
-        if (config.subscriptionId == 0) {}
+        if (config.subscriptionId == 0) {
+            CreateSubscription createSubScript = new CreateSubscription();
+            (config.subscriptionId, config.vrfCoordinator) = createSubScript
+                .createSubscription(config.vrfCoordinator);
+        }
 
         vm.startBroadcast();
         Lottery lottery = new Lottery(
